9ddfa881b1536217dbfaa373d83246eb63537c71
PedroLilo <pedrohenriquelilo557@gmail.com>
2025-06-27 17:23:37 -0300

    inicio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculadora</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body style='background-color:#c9df8a'>
+    <header>
+        <div class="navbar shadow-sm" style='background-color:#d2a6f9'>
+        <div class="d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <img src="{{ asset('storage/imagens/logo.png') }}" width="225" height="60" viewBox="0 0 54 54" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
+          </a>
+          <nav class="site-header align-itens-center py-1">
+            <div class="container d-flex flex-column flex-md-row justify-content-between">
+                <a href="/">Porra</a>
+            </div>
+          </nav>
+
+        
+        </div>
+    </header>
+</body>
+</html>
